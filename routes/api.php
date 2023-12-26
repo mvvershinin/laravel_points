e73f8eaae99c00f1cdb714c55efc657388782700
Mikhail Vershinin <mvvershinin@gmail.com>
2023-12-26 23:50:10 +0700

    init commit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test', function () {
+    \Illuminate\Support\Facades\Log::debug('test debug log');
+    return ['test'];
+});
